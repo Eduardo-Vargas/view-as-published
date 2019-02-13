c694326360cff316dccce469edf31958129c6ee2
Eduardo Vargas <evargas9@gatech.edu>
2019-02-13 14:26:05 -0500

    updated extension to toggle back to authoring mode

--- background.js
+++ background.js
@@ -6,8 +6,46 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
   chrome.tabs.query(queryInfo, function(tabs) {
     var tab = tabs[0];
-    var url = tab.url+'?wcmmode=disabled';
-    var temp = url.replace('editor.html/', '');
-    window.open(temp, '_blank');
+
+    if (tab.url.includes('editor.html/')) {
+      var url = tab.url + '?wcmmode=disabled';
+      var finalURL = url.replace('editor.html/', '');
+      chrome.tabs.update(tab.id, { url: finalURL });
+      chrome.browserAction.setIcon({
+        path: '/images/preview.png',
+        tabId: tab.id
+      });
+    } else {
+      var url = tab.url;
+      var temp = url.replace('?wcmmode=disabled', '');
+      var finalURL = temp.replace('.com/', '.com/editor.html/');
+      chrome.tabs.update(tab.id, { url: finalURL });
+      chrome.browserAction.setIcon({ path: '/images/dev.png', tabId: tab.id });
+    }
   });
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  if (request.changeIcon) {
+    chrome.tabs.query(queryInfo, function(tabs) {
+      var tab = tabs[0];
+
+      if (tab.url.includes('editor.html/')) {
+        chrome.browserAction.setIcon({
+          path: '/images/dev.png',
+          tabId: tab.id
+        });
+      } else {
+        chrome.browserAction.setIcon({
+          path: '/images/preview.png',
+          tabId: tab.id
+        });
+      }
+    });
+  }
 });
